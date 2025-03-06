2b2f331cb5a8f6c9641ed74ae6f4647547dda35a
Victória Marques <154396996+victoriacmarques@users.noreply.github.com>
2025-03-06 02:14:40 +0000

    criando classe App

---
+++  projeto1a/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    System.out.println("\nHello!");
+    String version = System.getProperty("java.version");
+    System.out.println("Running Java Version "+version+"\n");
+    }
+   }
