40509894bd0178b1813f6bc35dfd29d9880695b0
Victória Marques <154396996+victoriacmarques@users.noreply.github.com>
2025-03-06 02:22:39 +0000

    Criando classe App e Professor

---
+++ projeto1b/src/Professor.java
@@ -0,0 +1,25 @@
+public class Professor {
+    private String nome;
+    private int anosExperiencia;
+    public Professor(String nome, int anosExperiencia) {
+    this.nome = nome;
+    this.anosExperiencia = anosExperiencia;
+    }
+    public String getNome() {
+    return nome;
+    }
+    public int getAnosExperiencia() {
+    return anosExperiencia;
+    }
+    public String classifica() {
+    return switch(anosExperiencia){
+    case 0,1,2,3,4,5 -> "Assistente";
+    case 6,7,8,9,10 -> "Adjunto";
+    default -> "Titular";
+    };
+    }
+    @Override
+    public String toString() {
+    return "Professor: " + nome + ", anos de experiencia: " + anosExperiencia;
+    }
+   }
