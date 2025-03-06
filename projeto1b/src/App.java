f2183789056175e6ef4555c845e1e6d570b6dc24
Victória Marques <154396996+victoriacmarques@users.noreply.github.com>
2025-03-06 02:23:39 +0000

    Modificando classe App

--- projeto1b/src/App.java
+++ projeto1b/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+ public static void main(String args[]){
+ Scanner s = new Scanner(System.in);
+ System.out.print("Nome do professor? ");
+ String nome = s.next();
+ System.out.print("Quantos anos de experiencia ele tem? ");
+ int experiencia = s.nextInt();
+ Professor p = new Professor(nome,experiencia);
+ System.out.println(p.toString());
+ System.out.println("Categoria: "+p.classifica());
+ s.close();
+ }
 }
